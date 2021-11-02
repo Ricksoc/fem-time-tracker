376789278aef179e8c9e62edad029ce4ed8b9dc2
ricky <ricky.chaggar@gmail.com>
2021-11-02 16:28:02 +0000

    Add button event listener
    Add event listener to profile card buttons. Set to change active
    class to clicked button and remove from other buttons.

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,21 @@
 console.log("Javascript is running");
+
+const buttons = document.querySelectorAll(".profile-card-bottom__btn");
+
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    changeClass(event);
+  });
+});
+
+function changeClass(event) {
+
+  for (let i = 0; i<buttons.length; i++) {
+    if (parseInt(event.target.value) === i) {
+        buttons[i].classList.add("active");
+    } else {
+        buttons[i].classList.remove("active");
+    }
+  }
+}
