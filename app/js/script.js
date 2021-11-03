51227db613d65baa1314562cc602eb24281a6297
ricky <ricky.chaggar@gmail.com>
2021-11-03 16:36:49 +0000

    Read in data json
    Use fetch to read in json data file and display to console to check
    fetch has worked.

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,11 @@
 console.log("Javascript is running");
 
 const buttons = document.querySelectorAll(".profile-card-bottom__btn");
-
+const data = fetch("/data.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => console.log(data));
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (event) => {
@@ -10,12 +14,11 @@ buttons.forEach((btn) => {
 });
 
 function changeClass(event) {
-
-  for (let i = 0; i<buttons.length; i++) {
+  for (let i = 0; i < buttons.length; i++) {
     if (parseInt(event.target.value) === i) {
-        buttons[i].classList.add("active");
+      buttons[i].classList.add("active");
     } else {
-        buttons[i].classList.remove("active");
+      buttons[i].classList.remove("active");
     }
   }
 }
