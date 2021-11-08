155d78262bca166c5e281f9e702b06d1892fa0d5
ricky <ricky.chaggar@gmail.com>
2021-11-08 12:32:17 +0000

    Set up setPeriod function
    Create setPeriod as a function taking btn.innerHTML. Function to be
    used to change displayed html when timer period is changed on click.

--- app/js/script.js
+++ app/js/script.js
@@ -4,16 +4,21 @@ const buttons = document.querySelectorAll(".profile-card-bottom__btn");
 
 let JSONdata;
 
-
-getData()
-.then((data) => (JSONdata = data))
+getData().then((data) => (JSONdata = data));
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (event) => {
     changeClass(event);
+    let period = btn.innerHTML;
+    setPeriod(period);
   });
 });
 
+async function getData() {
+  const response = await fetch("/data.json");
+  const data = await response.json();
+  return data;
+}
 function changeClass(event) {
   for (let i = 0; i < buttons.length; i++) {
     if (parseInt(event.target.value) === i) {
@@ -24,8 +29,6 @@ function changeClass(event) {
   }
 }
 
-async function getData() {
-  const response = await fetch("/data.json");
-  const data = await response.json();
-  return data;
+function setPeriod(period) {
+  console.log(period);
 }
